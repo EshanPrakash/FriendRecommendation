911313149ad730f383211d2188ca1105d907b1a7
Eshan Prakash <148392140+EshanPrakash@users.noreply.github.com>
2023-12-03 22:46:07 -0800

    Complete functions
    list_users, add_user, add_friend, get_friends, suggest_friend

--- friend-recommender.py
+++ friend-recommender.py
@@ -14,7 +14,9 @@ class SocialNetwork:
         Returns:
             [str]: A list of usernames
         '''
-        pass # FIXME
+
+        return list(self.users.keys())
+
 
     def add_user(self, user):
         '''Add a user to the network
@@ -27,6 +29,10 @@ class SocialNetwork:
         Returns:
             None
         '''
+
+        if user not in self.users.keys():
+            self.users[user] = []
+
         pass # FIXME
 
     def add_friend(self, user, friend):
@@ -45,6 +51,13 @@ class SocialNetwork:
         Returns:
             None
         '''
+
+        if user not in self.users.keys():
+            self.add_user(user)
+        if friend not in self.users.values():
+            self.add_user(friend)
+        self.users[user].append(friend)
+
         pass # FIXME
 
     def get_friends(self, user):
@@ -57,7 +70,15 @@ class SocialNetwork:
             [str]: The list of usernames of the user's friends
 
         '''
-        pass # FIXME
+
+        friendlist = []
+
+        for i in enumerate(self.users.items()):
+            #print(i[1][0])
+            if i[1][0] == user:
+                friendlist = i[1][1]
+
+        return friendlist
 
     def suggest_friend(self, user):
         '''Suggest a friend to the user
@@ -70,7 +91,81 @@ class SocialNetwork:
         Returns:
             str: The username of a new candidate friend for the user
         '''
-        pass # FIXME
+
+        '''
+         1. Find another user that is the most similar to the specified user, based on the Jaccard index of their friends. 
+         2. Out of the most-similar-user's friends, find the one with the most followers that the specified user does not already follow.
+        '''
+        most_similar_user = ""
+        #user = "francis"
+        user_friends = []
+        not_user_friends = []
+        most_similar_user_friends = []
+        oldjaccard = 0
+        recommended_friend = ""
+
+        for i in enumerate(self.users.items()):
+            if i[1][0] == user:
+                user_friends = i[1][1]
+
+        for i in enumerate(self.users.items()):
+            if i[1][0] != user:
+                not_user_friends = i[1][1]
+
+            #print(user_friends)
+            #print(not_user_friends)
+
+            intersection = len(list(set(user_friends).intersection(not_user_friends)))
+            union = (len(user_friends) + len(not_user_friends)) - intersection
+            newjaccard = float(intersection) / union
+            #print("New Jaccard for " + str(i[1][0]) + ": " + str(newjaccard))
+            if newjaccard > oldjaccard:
+                most_similar_user = i[1][0]
+                oldjaccard = newjaccard
+            #print("Old Jaccard " + str(most_similar_user) + ": " + str(oldjaccard))
+
+        for i in enumerate(self.users.items()):
+            if i[1][0] == most_similar_user:
+                most_similar_user_friends = i[1][1]
+
+        unique_user_friends = list(set(user_friends) - set(most_similar_user_friends))
+        unique_most_similar_user_friends = list(set(most_similar_user_friends) - set(user_friends))
+
+        result = unique_user_friends + unique_most_similar_user_friends
+
+
+        #print(user)
+        #print(most_similar_user)
+        #print(user_friends)
+        #print(most_similar_user_friends)
+        #print(result)
+        #print()
+        oldcount = 0
+        for i in result:
+            for person in enumerate(self.users.items()):
+                if i == person[1][0]:
+                    person_friends = person[1][1]
+                    #print(person_friends)
+                    for friend in person_friends:
+                        newcount = sum(friend in friends_list for friends_list in self.users.values())
+                        #print(newcount)
+                        if newcount > oldcount:
+                            oldcount = newcount
+                            recommended_friend = friend
+                            #print(recommended_friend)
+                        #print(oldcount)
+                        #counts[friend] = count
+                        #print(f"{friend}: {count} times")
+                        #for person1 in enumerate(self.users.values()):
+                        #    print(person1[1])
+
+                    #for index in person_friends:
+                    #    print(index)
+                    #    for person1 in enumerate(self.users.items()):
+                    #        if index == person1[1][0]:
+                    #            print(len(person1[1][1]))
+
+        return recommended_friend
 
     def to_dot(self):
         result = []
